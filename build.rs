c5db36b12f9afeb0ac7a6b6da90788673b305aef
yoshy3 <yoshy3@gmail.com>
2026-02-01 14:34:00 +0900

    アイコンファイルの設定

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() {
     // ui/calendar.slint をコンパイルします
     slint_build::compile("ui/calendar.slint").unwrap();
+
+    // Windows アイコン設定
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
 }
